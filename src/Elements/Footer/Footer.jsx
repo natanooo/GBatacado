c17b94f428578b6aab38fc5677be4e529d708d52
natanooo <natannatansilva234@gmail.com>
2024-05-27 10:17:03 -0300

    segundo

--- src/Elements/Footer/Footer.jsx
+++ src/Elements/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import N from "../../images/N.png"
 function Footer() {
   return (
     <div className="Footer">
-      <h1>Desenvolvido Por <a href=""><img src={N} alt="" />atan</a></h1>
+      <h1>Desenvolvido Por <a href="https://www.instagram.com/natanooooo?igsh=MTR3bXBucDV6bzE4ZA=="><img src={N} alt="" />atan</a></h1>
       <h1>© 2024 Todos os direitos reservados.</h1>
     </div>
   );
